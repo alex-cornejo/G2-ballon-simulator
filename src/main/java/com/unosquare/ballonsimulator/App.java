d53ccf094a5616e347b5832f74aae9fe878437ce
Alex Cornejo <ale_jaca@hotmail.com>
2019-02-08 12:43:05 -0600

    display menu was added

--- src/main/java/com/unosquare/ballonsimulator/App.java
+++ src/main/java/com/unosquare/ballonsimulator/App.java
@@ -1,5 +1,8 @@
 package com.unosquare.ballonsimulator;
 
+import com.unosquare.ballonsimulator.model.Observatory;
+import com.unosquare.ballonsimulator.model.Observatory.ObservatoryEnum;
+
 /**
  * Hello world!
  */
@@ -12,6 +15,11 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Please choose one of the next options");
+        System.out.println("");
+        System.out.println("\r[1] Generate dataset randomly with a given size of records");
+        System.out.println("\r[2] Process a given dataset");
+
+
     }
 }
